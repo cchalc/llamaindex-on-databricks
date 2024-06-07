34e57e5a3aa6d575b5685700ca274d76d606a83d
cchalc <cchalc>
2024-06-07 08:13:56 +0000

    end of day tries

--- notebooks/databricks_rag_llamaindex_demo.py
+++ notebooks/databricks_rag_llamaindex_demo.py
@@ -1,16 +1,20 @@
 # Databricks notebook source
-# MAGIC %md #Using Databricks Foundation model APIs and Databricks Vector Search with LlamaIndex
+# MAGIC %md #Using OSS Models served from Marketplace and Databricks Vector Search with LlamaIndex
+
 # COMMAND ----------
 
 # MAGIC %pip install -r ../requirements.txt
-dbutils.library.restartPython()  # noqa
+# MAGIC dbutils.library.restartPython()  # noqa
+
 # COMMAND ----------
+
 from llama_index import ServiceContext, VectorStoreIndex, set_global_service_context
 
 from databricks_llamaindex.databricks_llm import DatabricksLLM, DatabricksEmbedding
 from databricks_llamaindex.databricks_vector_search import DatabricksVectorStore
 
 # COMMAND ----------
+
 context = dbutils.entry_point.getDbutils().notebook().getContext()  # noqa
 token = context.apiToken().get()
 host = context.apiUrl().get()
@@ -24,9 +28,10 @@ service_context = ServiceContext.from_defaults(
 set_global_service_context(service_context)
 
 # COMMAND ----------
+
 dvs = DatabricksVectorStore(
-    endpoint="one-env-shared-endpoint-0",
-    index_name="cjc.scratch.vci",
+    endpoint="one-env-shared-endpoint-8",
+    index_name="cjc.scratch.bbc_news_train_index",
     host=host,
     token=token,
     text_field="Text",
@@ -41,5 +46,9 @@ query_engine = dvs_index.as_query_engine()
 # COMMAND ----------
 
 # Query and print response
-response = query_engine.query("What is enron payout?")
+response = query_engine.query("What is Unity Catalog?")
 print(response)
+
+# COMMAND ----------
+
+
